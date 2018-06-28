3a1e8e34d8c8275802413d2f08251f1e69d5368a
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 16:17:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,10 @@ class CashRegister
 
   def add_item(item, price, quantity = 1)
     @total += price*quantity
-    @items << item
+    for quantity > 0
+      @items << item
+      quantity -= 1
+    end
     @last_transaction = price*quantity
   end
 
