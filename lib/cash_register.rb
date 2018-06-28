cc978a0e3056da5a0bcbf9f5360742e28e4084e2
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 16:08:20 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,6 @@
 
 class CashRegister
-  @
-
+  attr_accessor :discount, :total, :items
   def initialize(discount = 0)
     @discount = discount
     @total = 0
@@ -19,8 +18,13 @@ class CashRegister
 
   def apply_discount
     @discount == 0 ? str = "There is no discount to apply." : (@total *= (100-@discount)/100; str = "After the discount, the total comes to $#{@total}.")
-    
   end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
 
 
 end
