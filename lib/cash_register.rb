ad2d2d36854cfa00334270debd9313febc21bd3d
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 15:58:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,16 @@
 
 class CashRegister
+
   def initialize(discount=0)
     @discount = discount
     @total = 0
+  end
+  
+  def total
+    @total
+  end
+
+  def add_item(item, price)
+
   end
 end
