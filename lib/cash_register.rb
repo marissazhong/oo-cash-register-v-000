e7098f4f992eb1b467e1fb4e25c3be7a7f665302
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 15:58:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,12 +5,12 @@ class CashRegister
     @discount = discount
     @total = 0
   end
-  
+
   def total
     @total
   end
 
-  def add_item(item, price)
+  def add_item(item, price, quantity = 1)
 
   end
 end
